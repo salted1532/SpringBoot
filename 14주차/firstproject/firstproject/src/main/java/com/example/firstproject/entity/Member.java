7523e160ba0a6e8606e13c8b928de77ff9f5f2d9
Salted1532 <64358271+salted1532@users.noreply.github.com>
2025-12-18 16:12:17 +0900

    13~14주차 수업내용

---
+++ 14주차/firstproject/firstproject/src/main/java/com/example/firstproject/entity/Member.java
@@ -0,0 +1,40 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+/*    public Member(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }*/
+
+/*    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + email + '\'' +
+                ", content='" + password + '\'' +
+                '}';
+    }*/
+}
