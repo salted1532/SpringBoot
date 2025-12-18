e9643dc1bcf29e19271345b61ac416ae902234a7
Salted1532 <64358271+salted1532@users.noreply.github.com>
2025-12-18 16:05:12 +0900

    10주차 수업내용

---
+++ 10주차/firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,38 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+/*    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }*/
+}
